263e33794b1c8154f3db85be98c76f8b67a42f2c
Priyanka Arora <parora0711@gmail.com>
2024-08-04 14:02:12 +0530

    create and and OAuth component

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -14,8 +14,14 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true,
-    }
-}, {timestamps: true});
+    },
+    avatar:{
+        type: String,
+        default: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fvectors%2Fblank-profile-picture-mystery-man-973460%2F&psig=AOvVaw1aznrYr2LlB0apyU4Cc37L&ust=1722843449947000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCIDkzo7q2ocDFQAAAAAdAAAAABAE"
+    },
+}, 
+    {timestamps: true}
+);
 
 const User = mongoose.model('User',userSchema);
 
